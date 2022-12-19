cd9e1048081fcce4c6cb0a706afdb6f124b2887a
Viktoriia181818 <vcibulskaa4@gmail.com>
2022-12-19 09:31:18 +0200

    UPDATE TASK 5

--- js/task-04.js
+++ js/task-04.js
@@ -5,17 +5,13 @@ const oneButton = document.querySelector('#counter').lastElementChild;
 const twoButton = document.querySelector('#counter').firstElementChild;
 
 oneButton.addEventListener('click', el => {
-
     valueEl += 1;
-
     counterValue.textContent = valueEl;
 
 });
 
 twoButton.addEventListener('click', el => {
-
     valueEl -= 1;
-
     counterValue.textContent = valueEl;
 
 });
